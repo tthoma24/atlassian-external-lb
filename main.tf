b01a5b616ea7173a0a6ab8f3082df91110c944b3
Teddy Thomas <tjthomas292@gmail.com>
2021-02-22 15:52:08 -0500

    Add ELB config and target groups

--- main.tf
+++ main.tf
@@ -3,14 +3,48 @@ provider "aws" {
   version = "2.7.0"
 }
 
-resource "aws_eip" "app_us_east_1c" {
+resource "aws_eip" "jira-public_us_east_1c" {
   tags = {
-    "Name" = "app_us_east_1c"
+    "Name" = "jira-public_us_east_1c"
   }
 }
 
-resource "aws_eip" "app_us_east_1d" {
+resource "aws_eip" "jira-public_us_east_1d" {
   tags = {
-    "Name" = "app_us_east_1d"
+    "Name" = "jira-public_us_east_1d"
   }
+}
+
+resource "aws_lb" "jira_public" {
+  name                       = "jira-public"
+  load_balancer_type         = "network"
+  internal                   = false
+
+  subnet_mapping {
+    subnet_id     = var.public_subnet_us_east_1c
+    allocation_id = aws_eip.jira-public_us_east_1a.ic
+  }
+
+  subnet_mapping {
+    subnet_id     = var.public_subnet_us_east_1d
+    allocation_id = aws_eip.jira-public_us_east_1d.id
+  }
+}
+
+resource "aws_lb_target_group" "jira_public_80" {
+  name              = "jira-public-80"
+  port              = 80
+  protocol          = "TCP"
+  target_type       = "ip"
+  vpc_id            = var.vpc_id
+  proxy_protocol_v2 = false
+}
+
+resource "aws_lb_target_group" "jira_public_443" {
+  name              = "jira-public-443"
+  port              = 443
+  protocol          = "TCP"
+  target_type       = "ip"
+  vpc_id            = var.vpc_id
+  proxy_protocol_v2 = false
 }
