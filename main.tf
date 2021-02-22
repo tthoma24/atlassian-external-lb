39dc0d2764a287a1f9c89024706d939a4a8b1943
Teddy Thomas <tjthomas292@gmail.com>
2021-02-22 15:48:14 -0500

    Allocate EIPs

--- main.tf
+++ main.tf
@@ -2,3 +2,14 @@ provider "aws" {
   region = var.aws_region
   version = "2.7.0"
 }
+
+resource "aws_eip" "app_us_east_1c" {
+  tags = {
+    "Name" = "app_us_east_1c"
+  }
+}
+
+resource "aws_eip" "app_us_east_1d" {
+  tags = {
+    "Name" = "app_us_east_1d"
+  }
