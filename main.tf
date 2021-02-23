a55cc1b4bf4ef724c7d4151e5876f006cf55ed8a
Teddy Thomas <tjthomas292@gmail.com>
2021-02-23 10:27:47 -0500

    Add S3 and IAM for Lambda function

--- main.tf
+++ main.tf
@@ -76,3 +76,98 @@ resource "aws_lb_listener" "jira_public_443" {
   }
 }
 
+resource "aws_s3_bucket" "jira_static_lb" {
+  bucket = "bh-jira-static-lb"
+  acl    = "private"
+  region = "us-east-1"
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_iam_role_policy" "jira_static_lb_lambda" {
+  name = "jira_static-lb-lambda"
+  role = aws_iam_role.jira_static_lb_lambda.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": [
+        "arn:aws:logs:*:*:*"
+      ],
+      "Effect": "Allow",
+      "Sid": "LambdaLogging"
+    },
+    {
+      "Action": [
+        "s3:GetObject",
+        "s3:PutObject"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.static_lb.arn}/*"
+      ],
+      "Effect": "Allow",
+      "Sid": "S3"
+    },
+    {
+      "Action": [
+        "elasticloadbalancing:RegisterTargets",
+        "elasticloadbalancing:DeregisterTargets"
+      ],
+      "Resource": [
+        "${aws_lb_target_group.jira_public_80.arn}",
+        "${aws_lb_target_group.jira_public_443.arn}"
+      ],
+      "Effect": "Allow",
+      "Sid": "ChangeTargetGroups"
+    },
+    {
+      "Action": [
+        "elasticloadbalancing:DescribeTargetHealth"
+      ],
+      "Resource": "*",
+      "Effect": "Allow",
+      "Sid": "DescribeTargetGroups"
+    },
+    {
+      "Action": [
+        "cloudwatch:putMetricData"
+      ],
+      "Resource": "*",
+      "Effect": "Allow",
+      "Sid": "CloudWatch"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "jira_static_lb_lambda" {
+  name        = "jira-static-lb-lambda"
+  description = "Managed by Terraform"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
